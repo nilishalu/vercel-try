8fd79dcac4c73510b7abc3254dd83a3248ed465a
nilishalu <shalini.c.e.ganu@gmail.com>
2024-01-30 01:22:37 +0530

    added some fucntionality

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 // index.js
 const express = require('express')
+const path = require("path");
 const app = express()
 const PORT = 4000
 
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get('/home', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
 })
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "/views/index.html"))
+})
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
